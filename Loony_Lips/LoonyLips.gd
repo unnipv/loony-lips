9f3253b87b02e3910d894d5f57007c4bb6e7456f
unnipv <unnimonpv@gmail.com>
2021-02-17 21:49:06 +0530

    updates

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -2,4 +2,6 @@ extends Control
 
 
 func _ready():
-	print("Victoria Concordia Crescit")
+	var prompts = ["Little Hangleton","the Riddle House","Riddle family"]
+	var story = "The villagers of %s still called it ‘%s', even though it had been many years since the %s had lived there."
+	print(story % prompts)
