21bc35d85ddb7cdbc33cf2b3760ac32d61d4e89c
unnipv <unnimonpv@gmail.com>
2021-02-18 10:04:51 +0530

    Taking text input from player

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -4,4 +4,12 @@ extends Control
 func _ready():
 	var prompts = ["Little Hangleton","the Riddle House","Riddle family"]
 	var story = "The villagers of %s still called it ‘%s', even though it had been many years since the %s had lived there."
-	print(story % prompts)
+	$VBoxContainer/DisplayText.text = story % prompts
+
+	
+func _on_InputText_text_entered(new_text):
+	update_DisplayText(new_text)
+
+func update_DisplayText(new_text):
+	$VBoxContainer/DisplayText.text = new_text
+	$VBoxContainer/InputText.clear()
